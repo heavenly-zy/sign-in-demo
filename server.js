bf5d6d4deb23cbec051c8cbfbe21537f27572d2d
heavenly-zy <1457715339@qq.com>
2019-11-25 00:21:04 +0800

    后端指定setHeader类型为JSON

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ var server = http.createServer(function (request, response) {
       let { email, password, password_confirmation } = hash
       if (email.indexOf('@') === -1) { // 用户输入邮箱中不存在'@'字符
         response.statusCode = 400
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
         response.write(`{
           "errors": {
             "email": "invalid"
